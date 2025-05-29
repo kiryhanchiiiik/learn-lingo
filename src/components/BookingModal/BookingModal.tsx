cfa7d4baed9106e62259b104d048a46f4d60fdb5
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-05-29 12:35:19 +0300

    add BookingModal component and add logic

---
+++ src/components/BookingModal/BookingModal.tsx
@@ -0,0 +1,182 @@
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+import css from "./BookingModal.module.scss";
+import Modal from "../Modal/Modal";
+import { toast } from "react-toastify";
+
+interface BookScheme {
+  reason: string;
+  name: string;
+  email: string;
+  phone: string;
+}
+
+interface Teacher {
+  avatar_url: string;
+  name: string;
+  surname: string;
+  lessons_done: number;
+  rating: number;
+  price_per_hour: number;
+  languages: string;
+  lesson_info: string;
+  conditions: string;
+  moreInfo: string;
+  levels: string[];
+}
+interface Props {
+  teacher: Teacher;
+  onClose: () => void;
+}
+
+const validationSchema = Yup.object().shape({
+  reason: Yup.string().required("Please select a reason"),
+  name: Yup.string()
+    .min(3, "Too short!")
+    .max(20, "Too long!")
+    .required("Required!"),
+  email: Yup.string()
+    .email("Email must be a valid format")
+    .required("Required!"),
+  phone: Yup.string()
+    .matches(/^\+?[0-9\s\-]{7,15}$/, "Enter a valid phone number")
+    .required("Required!"),
+});
+
+const BookingModal = ({ teacher, onClose }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+    reset,
+  } = useForm<BookScheme>({
+    resolver: yupResolver(validationSchema),
+    defaultValues: {
+      reason: "",
+      name: "",
+      email: "",
+      phone: "",
+    },
+  });
+
+  const onSubmit = (data: BookScheme) => {
+    console.log("Booking data", data);
+    toast.success("Booking successful!");
+    reset();
+    onClose();
+  };
+
+  return (
+    <Modal onClose={onClose}>
+      <div className={css.bookContainer}>
+        <div className={css.bookDescContainer}>
+          <h2 className={css.bookTrialTitle}>Book trial lesson</h2>
+          <p className={css.bookTrialSubTitle}>
+            Our experienced tutor will assess your current language level,
+            discuss your learning goals, and tailor the lesson to your specific
+            needs.
+          </p>
+        </div>
+
+        <div className={css.imageTextContainer}>
+          <img
+            className={css.avatar}
+            src={teacher.avatar_url}
+            alt={teacher.name}
+          />
+          <div className={css.teacherInfoContainer}>
+            <p className={css.teacherName}>Your teacher</p>
+            <p className={css.teacherNameSurname}>
+              {teacher.name} {teacher.surname}
+            </p>
+          </div>
+        </div>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2>What is your main reason for learning English?</h2>
+          <Controller
+            name="reason"
+            control={control}
+            render={({ field }) => (
+              <div className={css.radioGroup}>
+                {[
+                  "Career and business",
+                  "Lesson for kids",
+                  "Living abroad",
+                  "Exams and coursework",
+                  "Culture, travel or hobby",
+                ].map((option) => (
+                  <label key={option} className={css.radioLabel}>
+                    <input
+                      type="radio"
+                      value={option}
+                      checked={field.value === option}
+                      onChange={() => field.onChange(option)}
+                    />
+                    <span className={css.customRadio}>
+                      <span className={css.radioDot}></span>
+                    </span>
+                    <span className={css.radioText}>{option}</span>
+                  </label>
+                ))}
+              </div>
+            )}
+          />
+          {errors.reason && (
+            <p className={css.error}>{errors.reason.message}</p>
+          )}
+
+          <div className={css.formGroup}>
+            <label>
+              Name
+              {errors.name && (
+                <span className={css.error}> - {errors.name.message}</span>
+              )}
+            </label>
+            <input
+              className={css.input}
+              {...register("name")}
+              placeholder="Full Name"
+            />
+          </div>
+
+          <div className={css.formGroup}>
+            <label>
+              Email
+              {errors.email && (
+                <span className={css.error}> - {errors.email.message}</span>
+              )}
+            </label>
+            <input
+              className={css.input}
+              {...register("email")}
+              placeholder="Email"
+            />
+          </div>
+
+          <div className={css.formGroup}>
+            <label>
+              Phone
+              {errors.phone && (
+                <span className={css.error}> - {errors.phone.message}</span>
+              )}
+            </label>
+            <input
+              className={css.input}
+              {...register("phone")}
+              placeholder="Phone number"
+            />
+          </div>
+
+          <button className={css.bookBtn} type="submit">
+            Book
+          </button>
+        </form>
+      </div>
+    </Modal>
+  );
+};
+
+export default BookingModal;
--- src/pages/TeachersPage/TeachersPage.tsx
+++ src/pages/TeachersPage/TeachersPage.tsx
@@ -8,14 +8,11 @@ import {
   removeFavorite,
   setFavorites,
 } from "../../redux/favorites/favoritesSlice";
-import TeacherCard from "../../components/TeacherCard/TeacherCard";
-import css from "./TeachersPage.module.scss";
-import "react-toastify/dist/ReactToastify.css";
 import { toast, Bounce } from "react-toastify";
-import Modal from "../../components/Modal/Modal";
-import { Controller, useForm } from "react-hook-form";
-import * as Yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
+import TeacherCard from "../../components/TeacherCard/TeacherCard";
+import BookingModal from "../../components/BookingModal/BookingModal";
+import "react-toastify/dist/ReactToastify.css";
+import css from "./TeachersPage.module.scss";
 
 interface Review {
   reviewer_name: string;
@@ -38,27 +35,6 @@ interface Teacher {
   levels: string[];
 }
 
-interface BookScheme {
-  reason: string;
-  name: string;
-  email: string;
-  phone: string;
-}
-
-const validationSchema = Yup.object().shape({
-  reason: Yup.string().required("Please select a reason"),
-  name: Yup.string()
-    .min(3, "Too short!")
-    .max(20, "Too long!")
-    .required("Required!"),
-  email: Yup.string()
-    .email("Email must be a valid format")
-    .required("Required!"),
-  phone: Yup.string()
-    .matches(/^\+?[0-9\s\-]{7,15}$/, "Enter a valid phone number")
-    .required("Required!"),
-});
-
 const TeachersPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedTeacher, setSelectedTeacher] = useState<Teacher | null>(null);
@@ -69,22 +45,6 @@ const TeachersPage = () => {
   const dispatch = useDispatch();
   const user = useSelector((state: RootState) => state.auth.user);
   const favorites = useSelector((state: RootState) => state.favorites.items);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-    control,
-  } = useForm<BookScheme>({
-    resolver: yupResolver(validationSchema),
-    defaultValues: {
-      reason: "",
-      name: "",
-      email: "",
-      phone: "",
-    },
-  });
 
   useEffect(() => {
     const fetchTeachers = async () => {
@@ -155,18 +115,6 @@ const TeachersPage = () => {
     setIsModalOpen(true);
   };
 
-  const onSubmit = async (data: BookScheme) => {
-    try {
-      console.log("Booking data", data);
-      toast.success("Booking successful!");
-      reset();
-      setIsModalOpen(false);
-    } catch (error) {
-      console.log(error);
-      toast.error("Failed to book lesson");
-    }
-  };
-
   return (
     <section className={css.teachers}>
       <FilterForm />
@@ -193,121 +141,10 @@ const TeachersPage = () => {
       </ul>
 
       {isModalOpen && selectedTeacher && (
-        <Modal onClose={() => setIsModalOpen(false)}>
-          <div className={css.bookContainer}>
-            <div className={css.bookDescContainer}>
-              <h2 className={css.bookTrialTitle}>Book trial lesson</h2>
-              <p className={css.bookTrialSubTitle}>
-                Our experienced tutor will assess your current language level,
-                discuss your learning goals, and tailor the lesson to your
-                specific needs.
-              </p>
-            </div>
-
-            <div className={css.imageTextContainer}>
-              <img
-                className={css.avatar}
-                width={44}
-                height={44}
-                src={selectedTeacher.avatar_url}
-                alt={selectedTeacher.name}
-              />
-              <div className={css.teacherInfoContainer}>
-                <p className={css.teacherName}>Your teacher</p>
-                <p className={css.teacherNameSurname}>
-                  {selectedTeacher.name} {selectedTeacher.surname}
-                </p>
-              </div>
-            </div>
-
-            <div className={css.radioContainer}>
-              <h2>What is your main reason for learning English?</h2>
-
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <Controller
-                  name="reason"
-                  control={control}
-                  render={({ field }) => (
-                    <div className={css.radioGroup}>
-                      {[
-                        "Career and business",
-                        "Lesson for kids",
-                        "Living abroad",
-                        "Exams and coursework",
-                        "Culture, travel or hobby",
-                      ].map((option) => (
-                        <label key={option} className={css.radioLabel}>
-                          <input
-                            type="radio"
-                            value={option}
-                            checked={field.value === option}
-                            onChange={() => field.onChange(option)}
-                          />
-                          <span className={css.customRadio}>
-                            <span className={css.radioDot}></span>
-                          </span>
-                          <span className={css.radioText}>{option}</span>
-                        </label>
-                      ))}
-                    </div>
-                  )}
-                />
-                {errors.reason && (
-                  <p className={css.error}>{errors.reason.message}</p>
-                )}
-
-                <div className={css.formGroup}>
-                  <div className={css.labelWrapper}>
-                    <label>Name</label>
-                    {errors.name && (
-                      <span className={css.error}>{errors.name.message}</span>
-                    )}
-                  </div>
-                  <input
-                    id="name"
-                    className={css.input}
-                    type="text"
-                    {...register("name")}
-                    placeholder="Full Name"
-                  />
-                </div>
-
-                <div>
-                  <div className={css.labelWrapper}>
-                    <label>Email</label>
-                    {errors.email && (
-                      <span className={css.error}>{errors.email.message}</span>
-                    )}
-                  </div>
-                  <input
-                    className={css.input}
-                    type="text"
-                    {...register("email")}
-                    placeholder="Email"
-                  />
-                </div>
-
-                <div className={css.lastInput}>
-                  <div className={css.labelWrapper}>
-                    <label>Phone</label>
-                    {errors.phone && (
-                      <span className={css.error}>{errors.phone.message}</span>
-                    )}
-                  </div>
-                  <input
-                    className={css.input}
-                    type="text"
-                    {...register("phone")}
-                    placeholder="Phone number"
-                  />
-                </div>
-                <button className={css.bookBtn} type="submit">
-                  Book
-                </button>
-              </form>
-            </div>
-          </div>
-        </Modal>
+        <BookingModal
+          teacher={selectedTeacher}
+          onClose={() => setIsModalOpen(false)}
+        />
       )}
 
       {visibleCount < teachers.length && (
