fa6e451fd24648650eb63cceaae1e980c4da17cf
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-06-21 12:48:33 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "@/styles/variables/variables" as *;`,
-      },
-    },
-  },
 });
